3e38635485536e5ada5aebf8edc0507116d87eae
UdaraDewshan <dewudara@gmail.com>
2025-11-10 21:40:49 +0530

    Add All characteres image

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,3 +4,24 @@ runSound.loop = true;
 var jumpSound = new Audio("asset/sound/jump.mp3");
 
 var deadSound = new Audio("asset/sound/dead.mp3");
+
+
+
+
+
+var playerID = document.getElementById("player");
+
+var runWorkId = 0;
+var runImageNumber = 1;
+
+function run(){
+    runImageNumber++;
+
+    if(runImageNumber == 9){
+        runImageNumber = 1;
+    }
+
+    playerID.src = "asset/image/Run (" + runImageNumber + ").png";
+
+}
+
