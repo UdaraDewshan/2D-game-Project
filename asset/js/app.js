c9c39e4376f46169c4d72df7fffb52ad85a9d38b
UdaraDewshan <dewudara@gmail.com>
2025-12-23 23:36:37 +0530

    Add run function

--- asset/js/app.js
+++ asset/js/app.js
@@ -6,12 +6,44 @@ var jumpSound = new Audio("asset/sound/jump.mp3");
 var deadSound = new Audio("asset/sound/dead.mp3");
 
 
+function keyCheck(event){
 
+    //enter key
+    if(event.which == 13){
+
+
+        if(runWorkerId == 0){
+           runWorkerId = setInterval(run,100);
+            runSound.play();
+
+           moveBackgroundWorkerId = setInterval(moveBackground,100);
+           scoreWorkerId = setInterval(updateScore,50);
+           creatBlockWorkerId = setInterval(creatBlock,100);
+           moveBlockWorkerId =  setInterval(moveBlock,100);
+        }
+    }
+
+    //space key
+    if(event.which == 32){
+
+        if (jumpWorkerId == 0){
+
+            clearInterval(runWorkerId);
+            runSound.pause();
+
+            runWorkerId = -1;
+
+            jumpWorkerId = setInterval(jump, 100);
+            jumpSound.play();
+
+        }
+    }
+}
 
 
 var playerID = document.getElementById("player");
 
-var runWorkId = 0;
+var runWorkerId = 0;
 var runImageNumber = 1;
 
 function run(){
@@ -24,4 +56,6 @@ function run(){
     playerID.src = "asset/image/Run (" + runImageNumber + ").png";
 
 }
+
+
 
