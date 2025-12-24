60ca76f49cd2ac60b8c81e4770316ef6a797258c
UdaraDewshan <dewudara@gmail.com>
2025-12-24 23:55:12 +0530

    Add jump function

--- asset/js/app.js
+++ asset/js/app.js
@@ -79,3 +79,61 @@ function updateScore(){
     newScore++;
     score.innerHTML = newScore;
 }
+
+
+//jump
+var jumpImageNumber = 1;
+var jumpWorkerId = 0;
+var playerMarginTop = 390;
+
+function jump(){
+   jumpImageNumber++;
+   //fly
+   if(jumpImageNumber <= 7){ //jump images 2-7
+
+    playerMarginTop = playerMarginTop - 40;
+    playerId.style.marginTop = playerMarginTop +"px";
+
+   }
+
+   //land
+   if(jumpImageNumber >= 8){ //jump image 8-1
+
+    playerMarginTop = playerMarginTop + 40;
+    playerId.style.marginTop = playerMarginTop + "px";
+
+   }
+  
+
+
+   if(jumpImageNumber ==13){
+      jumpImageNumber = 1;
+
+      clearInterval(jumpWorkerId);
+      jumpWorkerId = 0;
+
+      runWorkerId = setInterval(run,100);
+      runSound.play();
+      
+
+      if(scoreWorkerId==0){
+         scoreWorkerId = setInterval(updateScore,100);
+      }
+
+
+      if(moveBackgroundWorkerId == 0){
+        moveBackgroundWorkerId = setInterval(moveBackground,100);
+      }
+
+      if(creatBlockWorkerId == 0){
+        creatBlockWorkerId = setInterval(creatBlock,100);
+      }
+
+      if(moveBlockWorkerId==0){
+         moveBlockWorkerId = setInterval(moveBlock,100);
+      }
+
+   }
+
+   playerId.src= "Jump (" + jumpImageNumber + ").png";
+}
